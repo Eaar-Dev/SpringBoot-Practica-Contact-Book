7fecc4e1108185e6e27ba6d95e1eb385219b5db2
Edgar Alejandro Alcocer Reyes <aspire5@outlook.es>
2025-09-30 15:03:14 -0600

    First commit with starter

---
+++ spring-contact-book/src/main/java/contact/book/spring_contact_book/SpringContactBookApplication.java
@@ -0,0 +1,13 @@
+package contact.book.spring_contact_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContactBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContactBookApplication.class, args);
+	}
+
+}
